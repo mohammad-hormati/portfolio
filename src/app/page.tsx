a12aa52cd2acb13a5f7024ffc0dbf2e24877564f
Mohammad <admin@admins-MacBook-Pro.local>
2026-08-02 06:19:42 +0330

    fix: delete blog section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import AboutSection from "@/components/sections/AboutSection";
-import BlogSection from "@/components/sections/BlogSection";
 import ContactSection from "@/components/sections/ContactSection";
 import HeroSection from "@/components/sections/HeroSection";
 import ProjectsSection from "@/components/sections/ProjectsSection";
@@ -12,7 +11,6 @@ export default function Home() {
       <AboutSection />
       <ProjectsSection />
       <SkillsSection />
-      <BlogSection />
       <ContactSection />
     </main>
   );
